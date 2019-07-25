575443cbbb719e5d790bbd9eaa445b3207fcbc71
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-25 10:39:55 -0400

    Cleaned up the code and displayed the array in a more abbreviate style

--- CalculateAvgTestScores.proj/Program.cs
+++ CalculateAvgTestScores.proj/Program.cs
@@ -5,28 +5,29 @@ namespace CalculateAvgTestScores.proj {
         static void Main(string[] args) {
             Console.WriteLine("Calculate the average of test scores");
 
-            int[] scores = new int[12];
-                scores[0] = 71;
-                scores[1] = 78;
-                scores[2] = 89;
-                scores[3] = 73;
-                scores[4] = 68;
-                scores[5] = 73;
-                scores[6] = 96;
-                scores[7] = 71;
-                scores[8] = 72;
-                scores[9] = 65;
-                scores[10] = 87;
-                scores[11] = 63;
+            //int[] scores = new int[12];
+            //    scores[0] = 71;
+            //    scores[1] = 78;
+            //    scores[2] = 89;
+            //    scores[3] = 73;
+            //    scores[4] = 68;
+            //    scores[5] = 73;
+            //    scores[6] = 96;
+            //    scores[7] = 71;
+            //    scores[8] = 72;
+            //    scores[9] = 65;
+            //    scores[10] = 87;
+            //    scores[11] = 63;
 
-                int total = 0;
-                int index = 0;
-               // int avg = total / index;
+            //    int total = 0;
+            //    int index = 0;
+
+
+            int[] scores = new int[12] { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63 }; // Quicker method
 
             while (index < 12) {                        // less than the total number
                 total = total + scores[index];
                 index = index + 1;                      // without this - an infinite loop would be created
-                //avg = total / index;
             }
             //int avg = total / index;                // This could cause a problem if another user set the index to 0
             //int avg = total / 12;
@@ -34,7 +35,7 @@ namespace CalculateAvgTestScores.proj {
                                  // when changed to a decimal atleast one number in in calculation must be decimal
 
             //Console.WriteLine("The average test score is " + avg + "%");
-            Console.WriteLine($"The average test score is {avg}%");         // another wat to write it
+            Console.WriteLine($"The average test score is {avg}%");         // another way to write it
 
 
 
